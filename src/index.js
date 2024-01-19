4666b3326ece5fa576897353eb3f2bae302cbd68
darialiit <solomkina.daria@gmail.com>
2024-01-19 11:28:57 -0300

    create projects on btn click

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 import './style.css';
+
+class Project {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+const projectsLog = [];
+
+function createProject(name) {
+  const newProject = new Project(name);
+  projectsLog.push(newProject);
+  console.log(projectsLog);
+  renderProject(newProject);
+}
+
+const projectsDiv = document.querySelector('.projects');
+
+function renderProject(project) {
+  const newDiv = document.createElement('div');
+  newDiv.textContent = project.name;
+  projectsDiv.appendChild(newDiv);
+}
+
+const btnAdd = document.querySelector('#add-project');
+
+btnAdd.addEventListener('click', () => {
+  const answer = prompt('What is the name of the project?');
+  createProject(answer);
+});
